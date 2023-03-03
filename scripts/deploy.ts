f5576a59f7ef2f70a246c79aa5a6177656ef83ab
Rafael450 <rafaelgamer450@gmail.com>
2023-03-03 14:53:03 -0700

    ci: update open zeppelin

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -9,7 +9,15 @@ async function main() {
   // Get the ContractFactories and Signers here.
   const Sports = await ethers.getContractFactory("Sports");
   // deploy contracts
-  const sports = await Sports.deploy();
+  const sports = await Sports.deploy("0x326C977E6efc84E512bB9C30f76E30c160eD06FB", 
+    "0xCC79157eb46F5624204f47AB42b3906cAA40eaB7", 
+    "0x6361393833363663633733313439353762386330313263373266303561656562",
+    "0x326C977E6efc84E512bB9C30f76E30c160eD06FB",
+    "0x07865c6E87B9F70255377e024ace6630C1Eaa37F",
+    [], 
+    ["0x3B02935B6717b012f8240AA3a3A1Be0eCD37B315"], 
+    "https://www.random.org/integers/?num=1&min=1&col=1&base=10&format=plain&rnd=new&max=",
+    "https://www.random.org/integers/?num=1&min=1&col=1&base=10&format=plain&rnd=new&max=");
   console.log("Contract address:", sports.address);
 
   // Save copies of each contracts abi and address to the frontend.
