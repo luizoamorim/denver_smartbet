0ca9530916c4cfa6c409fad496373c117950025a
Luiz <luiz.oamorim@gmail.com>
2023-03-03 11:25:45 -0700

    chore: setup env vars

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -27,10 +27,6 @@ export default function Home() {
     };
 
     const makeBet = async () => {
-        // const provider = new ethers.JsonRpcProvider(
-        //   `https://goerli.infura.io/v3/${myInfuraId}`
-        // );
-
         const web3 = new Web3(magic.rpcProvider as any);
         const address = (await web3.eth.getAccounts())[0];
         console.log("ADDRESS: ", address);
--- frontend/src/utils/magic.ts
+++ frontend/src/utils/magic.ts
@@ -3,7 +3,11 @@ import { Magic } from "magic-sdk";
 let magic;
 
 if (typeof window !== "undefined") {
-    magic = new Magic(process.env.NEXT_PUBLIC_MAGIC_LINK_PK as string);
+    magic = new Magic(
+        process.env.NODE_ENV === "development"
+            ? (process.env.NEXT_PUBLIC_MAGIC_LINK_PK as string)
+            : (process.env.MAGIC_LINK_PK as string),
+    );
 }
 
 export default magic as Magic;
