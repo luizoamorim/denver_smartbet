d2d59ca6cdaa72cbb45b6547d820d774a4d362b2
Rafael450 <rafaelgamer450@gmail.com>
2023-03-03 14:16:25 -0700

    fix: fixed AccessControl functions

--- contracts/Sports.sol
+++ contracts/Sports.sol
@@ -114,14 +114,14 @@ contract Sports is ChainlinkClient, Ownable, AccessControl{
 
         // Admin Construct
         for (uint256 i = 0; i < _mods.length; ++i) {
-            _grantRole(MOD_ROLE, _mods[i]);
+            grantRole(MOD_ROLE, _mods[i]);
         }
 
         for (uint256 i = 0; i < _admins.length; ++i) {
-            _grantRole(ADMIN_ROLE, _admins[i]);
+            grantRole(ADMIN_ROLE, _admins[i]);
         }
 
-        _grantRole(RELAYER, _relayer);
+        grantRole(RELAYER, _relayer);
 
         numbersAPI = _numbersAPI;
         gamesAPI = _gamesAPI;
