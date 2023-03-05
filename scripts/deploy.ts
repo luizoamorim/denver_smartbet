9d7c17fb27bc01585d4d84463c41f82ed98e76ea
Luiz <luiz.oamorim@gmail.com>
2023-03-05 05:22:56 -0700

    chore: update bets after add a new one

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -10,13 +10,16 @@ async function main() {
   const Sports = await ethers.getContractFactory("Sports");
   // deploy contracts
   const sports = await Sports.deploy(
-    "0x326C977E6efc84E512bB9C30f76E30c160eD06FB", 
-    "0x40193c8518BB267228Fc409a613bDbD8eC5a97b3",
+    "0x326c977e6efc84e512bb9c30f76e30c160ed06fb",
+    [
+      "0x40193c8518bb267228fc409a613bdbd8ec5a97b3",
+      "0x7ca7215c6b8013f249a195cc107f97c4e623e5f5",
+    ],
     "0x6361393833363663633733313439353762386330313263373266303561656562",
     "0x3530636536353238663164623462376438383863616133666163323362373833",
     "0x6234616433323862323131663436626661303461623465313430323365363164",
-    "0x326C977E6efc84E512bB9C30f76E30c160eD06FB",
-    "0x0FA8781a83E46826621b3BC094Ea2A0212e71B23",
+    "0x326c977e6efc84e512bb9c30f76e30c160ed06fb",
+    "0x0fa8781a83e46826621b3bc094ea2a0212e71b23",
     [],
     [
       "0x3b02935b6717b012f8240aa3a3a1be0ecd37b315",
@@ -25,7 +28,7 @@ async function main() {
     [
       "https://www.random.org/integers/?num=1&min=1&col=1&base=10&format=plain&rnd=new&max=",
       "https://nbamockapi-git-main-luizoamorim.vercel.app/upcoming-games",
-      "https://nbamockapi-git-main-luizoamorim.vercel.app/resulting-games"
+      "https://nbamockapi-git-main-luizoamorim.vercel.app/resulting-games",
     ]
   );
   console.log("Contract address:", sports.address);
